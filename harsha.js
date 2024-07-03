945cb9a6117392568153c4ae55d798245c40f5d9
harshanm2000 <harshamavintop@gmail.com>
2024-07-03 20:58:03 +0530

    initial commit

---
+++ harsha.js
@@ -0,0 +1,52 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+
+    window.addEventListener('scroll', () => {
+        let scrollY = window.scrollY;
+
+        sections.forEach(section => {
+            let sectionTop = section.offsetTop - 150;
+            let sectionHeight = section.offsetHeight;
+            let sectionId = section.getAttribute('id');
+
+            if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+                });
+
+                document.querySelector(`header nav a[href="#${sectionId}"]`).classList.add('active');
+            }
+        });
+
+        let header = document.querySelector('header');
+        header.classList.toggle('sticky', scrollY > 100);
+
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    });
+});
+
+
+ScrollReveal ({
+
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content , .heading' ,{origin: 'top'});
+ScrollReveal().reveal('.home-img, .Skill-container ,.Project-box,Contact form',{origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img' ,{origin: 'left'});
+ScrollReveal().reveal('.home-content p, .about-content' ,{origin: 'right'});
+
+
+ 
